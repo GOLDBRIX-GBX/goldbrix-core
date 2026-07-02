85ed96c47ff978f33b5843a3607c9fe57c214a4a
GOLDBRIX-GBX <goldbrixgbx@protonmail.com>
2026-07-02 21:36:39 +0200

    S.6: mainnet DNS seed seed.goldbrix.app + clear inherited fixed seeds

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -194,7 +194,9 @@ LogPrintf("  hashMerkleRoot   = %s\n", genesis.hashMerkleRoot.ToString());
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-vSeeds.clear();
+        vFixedSeeds.clear(); // GOLDBRIX: no inherited fixed seeds
+        vSeeds.clear();
+        vSeeds.emplace_back("seed.goldbrix.app."); // GOLDBRIX mainnet DNS seed
 
 base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 25); // B...
 base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 63); // S....
